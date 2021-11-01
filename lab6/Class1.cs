3b5ab969e03014baed645ba8af5367bff6d251ad
Daniil Kachalkin <cara.by9@gmail.com>
2021-11-01 10:23:36 +0300

    edit

---
+++ lab6/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    public partial class Army
+    {
+        public void Add(ISmartCreature smartCreature)
+        {
+            list.Add(smartCreature);
+        }
+        public void Del(int a)
+        {
+            list.RemoveAt(a);
+        }
+        public void Info()
+        {
+            Console.WriteLine($"---------------------------Армия {Branch}--------------------------------");
+            foreach (var item in list)
+            {
+                item.Descrip();
+            }
+        }
+    }
+}
---
+++ lab6/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    public partial class Army
+    {
+        public string Branch { get; set; }
+        public List<ISmartCreature> list = new();
+        
+        public interface ISmartCreature
+        {
+            void Descrip();
+            int GetAge();
+            int GetPower();
+            string GetName();
+           
+        }
+        public class Human:ISmartCreature
+        {
+            public string name;
+            public int age;
+
+            public virtual void Descrip()
+            {
+                Console.WriteLine($"Солдат {name} {age} года рождения");
+            }
+
+            public int GetAge()
+            {
+                return age;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public virtual int GetPower()
+            {
+                return 0;
+            }
+        }
+        public class Transformer:Human,ISmartCreature
+        {
+            public int power;
+            public override void Descrip()
+            {
+                Console.WriteLine($"Трансформер {name} {age} года выпуска имеет мощность {power}");
+            }
+            public override int GetPower()
+            {
+                return power;
+            }
+
+        }
+        public class Controller
+        {
+            public void Age(Army army,int a)
+            {
+                Console.WriteLine($"Боевые единицы {a} года рождения(выпуска):");
+                foreach (var item in army.list)
+                {
+                    if (item.GetAge()==a)
+                    {
+                        item.Descrip();
+                    }
+                }
+            }
+            public void Power(Army army,int power)
+            {
+                foreach (var item in army.list)
+                {
+                    if (item is Transformer)
+                    {
+                        if (item.GetPower() == power)
+                        {
+                            Console.WriteLine(item.GetName());
+                        }
+                        else continue;
+                    }
+                    else continue;
+                }
+            }
+            public void HowMuch(Army army)
+            {
+                Console.WriteLine($"Количество боевых единиц в армии - {army.list.Count}");
+            }
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
